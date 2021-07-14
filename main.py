7e123d007c547aef51981e89e5498cc932ae474f
Ahmed Adil Ali <ahmed.ali@twosixtech.com>
2021-07-13 21:01:11 -0700

    python web scraper added

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+github_user = input('Input Github Username: ')
+url = 'http://github.com/'+github_user
+req = requests.get(url)
+soup = bs(req.content, 'html.parser')
+profile_pic = soup.find('img', {'alt': 'Avatar'})['src']
+print(profile_pic)
